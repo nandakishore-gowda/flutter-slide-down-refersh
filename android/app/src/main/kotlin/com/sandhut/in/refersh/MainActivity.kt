5342b7eee01f20b8444af1dc3a762df0e54002b6
NANDAKIAHORE GOWDA G <nandakishore01234@gmail.com>
2024-05-13 12:35:23 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/sandhut/in/refersh/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sandhut.`in`.refersh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:refersh/refresh.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(brightness: Brightness.dark),
+    );
+  }
+}
+
